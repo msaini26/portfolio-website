748d8b0addc9dada732f53491de8ad63b8dd7c95
Mansi S <mansisaini26@gmail.com>
2023-01-23 13:56:11 -0800

    nav + frame init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,10 @@
 <script>
   import Begin from '../lib/components/Begin.svelte';
+  import Frame from '../routes/Frame.svelte'
 </script>
 
 <!-- insert SEO title -->
 <Begin/> 
+
+<!-- insert header and footer -->
+<Frame/>
---
+++ src/routes/Frame.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Nav from '../lib/components/Nav.svelte'
+</script>
+
+<body>
+    <!-- import Nav bar component -->
+    <Nav/>
+
+</body>
+
