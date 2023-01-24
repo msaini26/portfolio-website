c68fe3840602831cc11bbb42cdcfaa64a44066ce
Mansi S <mansisaini26@gmail.com>
2023-01-24 15:14:24 -0800

    favicon included

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+		<!-- Bootstrap CSS -->
+		<link
+			rel="stylesheet"
+			href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css"
+			integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+			crossorigin="anonymous"
+		/>
+	</head>
+</html>
+
 <script>
   import Begin from '../lib/components/Begin.svelte';
   import Frame from '../routes/Frame.svelte'
 </script>
+
+
 
 <!-- insert SEO title -->
 <Begin/> 
