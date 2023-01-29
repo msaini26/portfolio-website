f545767eb2af25abbc62fd2c9642d392a2c11647
Mansi S <mansisaini26@gmail.com>
2023-01-28 18:00:36 -0800

    update path

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -49,12 +49,12 @@
 			<div class="row row-cols">
 				<div class="col-1">
 					<a href="https://www.linkedin.com/in/mansi-saini26/"
-						><img class="icon" alt="linkedin" src="/src/public/static/assets/linkedin.svg" /></a
+						><img class="icon" alt="linkedin" src="./src/public/static/assets/linkedin.svg" /></a
 					>
 				</div>
 				<div class="col-1">
 					<a href="https://github.com/msaini26"
-						><img class="icon" alt="github" src="/src/public/static/assets/github.svg" /></a
+						><img class="icon" alt="github" src="./src/public/static/assets/github.svg" /></a
 					>
 				</div>
 				<div class="col-1">
